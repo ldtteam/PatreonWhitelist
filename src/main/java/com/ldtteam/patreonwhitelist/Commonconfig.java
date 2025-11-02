73c2297ae947dda118df25dcb7f7fc9f2a291425
someaddons <someaddons@github.com>
2025-11-02 17:57:08 +0100

    add check for recently logged in players, to avoid checking the same player again as that stalls the server

--- src/main/java/com/ldtteam/patreonwhitelist/Commonconfig.java
+++ src/main/java/com/ldtteam/patreonwhitelist/Commonconfig.java
@@ -5,8 +5,14 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.event.config.ModConfigEvent;
 import net.neoforged.neoforge.common.ModConfigSpec;
 import net.neoforged.neoforge.event.server.ServerStoppingEvent;
+import org.apache.logging.log4j.Level;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -17,6 +23,9 @@ public class Commonconfig
     private ModConfigSpec.ConfigValue<List<? extends String>> loginTimes;
     private ModConfigSpec.ConfigValue<Integer>                offlineAuthValidHours;
 
+    /**
+     * Map of UUID to last successfull patreon auth time
+     */
     private Map<UUID, NameTime> lastLoginTimes = new HashMap<>();
     private DateTimeFormatter   timeFormatter  = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
     public  ModConfigSpec       holder         = null;
@@ -98,18 +107,114 @@ public class Commonconfig
     /**
      * Checks if the id has been authenticated recently
      *
-     * @param id
+     * @param profile
      * @return
      */
-    public boolean hasOfflineAuth(final UUID id)
+    public boolean checkAuth(final @NotNull GameProfile profile)
     {
+        final UUID id = profile.getId();
         final NameTime nameTime = lastLoginTimes.get(id);
         if (nameTime == null)
         {
-            return false;
+            return checkOnlineAuth(profile);
         }
 
-        return Duration.between(LocalDateTime.now(), nameTime.time).getSeconds() < offlineAuthValidHours.get() * 60L * 60L;
+        long seconds = Duration.between(LocalDateTime.now(), nameTime.time).getSeconds();
+        if (seconds < offlineAuthValidHours.get() * 60L * 60L)
+        {
+            if (seconds < 60 * 120)
+            {
+                // Skip auth request when last successful was less than two hours ago
+                Log.getLogger().log(Level.INFO, "Offline authenticated, recently logged in");
+                return true;
+            }
+            else
+            {
+                if (checkOnlineAuth(profile))
+                {
+                    return true;
+                }
+            }
+
+            Log.getLogger().log(Level.INFO, "Offline authenticated");
+            return true;
+        }
+        else
+        {
+            return checkOnlineAuth(profile);
+        }
+    }
+
+    /**
+     * Checks for authentication online and updates the config cache
+     *
+     * @param profile
+     * @return
+     */
+    private boolean checkOnlineAuth(final GameProfile profile)
+    {
+        if (this.checkUrl("https://auth.minecolonies.com/api/minecraft/" + profile.getId().toString() + "/whitelist"))
+        {
+            PatreonWhitelist.config.updateAuthFor(profile);
+            Log.getLogger().log(Level.INFO, "Online authenticated");
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean checkUrl(String urlString)
+    {
+        try
+        {
+            Log.getLogger().log(Level.INFO, "Patreon Whitelist validating, contacting API: " + urlString);
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setReadTimeout(5000);
+            conn.setConnectTimeout(5000);
+            int statusCode = conn.getResponseCode();
+            Log.getLogger().log(Level.INFO, "Patreon Whitelist validating, status code: " + statusCode);
+            conn.getInputStream();
+
+            String content;
+            try
+            {
+                BufferedReader input = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                try
+                {
+                    content = input.readLine();
+                }
+                catch (Throwable var15)
+                {
+                    try
+                    {
+                        input.close();
+                    }
+                    catch (Throwable var14)
+                    {
+                        var15.addSuppressed(var14);
+                    }
+
+                    throw var15;
+                }
+
+                input.close();
+            }
+            finally
+            {
+                conn.disconnect();
+            }
+
+            Log.getLogger().log(Level.INFO, "Patreon Whitelist validating, request content: " + content);
+            return statusCode == 200 && Boolean.parseBoolean(content);
+        }
+        catch (IOException var17)
+        {
+            var17.printStackTrace();
+            return false;
+        }
     }
 
     private static record NameTime(
--- src/main/java/com/ldtteam/patreonwhitelist/WhitelistOverride.java
+++ src/main/java/com/ldtteam/patreonwhitelist/WhitelistOverride.java
@@ -5,12 +5,7 @@ import net.minecraft.server.players.UserWhiteList;
 import org.apache.logging.log4j.Level;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class WhitelistOverride extends UserWhiteList
 {
@@ -22,79 +17,12 @@ public class WhitelistOverride extends UserWhiteList
     @Override
     public boolean isWhiteListed(@NotNull GameProfile profile)
     {
-        if (this.checkUrl("https://auth.minecolonies.com/api/minecraft/" + profile.getId().toString() + "/whitelist"))
-        {
-            PatreonWhitelist.config.updateAuthFor(profile);
-            Log.getLogger().log(Level.INFO, "Online authenticated");
-            return true;
-        }
-
         if (super.isWhiteListed(profile))
         {
             Log.getLogger().log(Level.INFO, "Whitelist authenticated");
             return true;
         }
 
-        if (PatreonWhitelist.config.hasOfflineAuth(profile.getId()))
-        {
-            Log.getLogger().log(Level.INFO, "Offline authenticated");
-            return true;
-        }
-
-        return false;
-    }
-
-    private boolean checkUrl(String urlString)
-    {
-        try
-        {
-            Log.getLogger().log(Level.INFO, "Patreon Whitelist validating, contacting API: " + urlString);
-            URL url = new URL(urlString);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setReadTimeout(5000);
-            conn.setConnectTimeout(5000);
-            int statusCode = conn.getResponseCode();
-            Log.getLogger().log(Level.INFO, "Patreon Whitelist validating, status code: " + statusCode);
-            conn.getInputStream();
-
-            String content;
-            try
-            {
-                BufferedReader input = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-
-                try
-                {
-                    content = input.readLine();
-                }
-                catch (Throwable var15)
-                {
-                    try
-                    {
-                        input.close();
-                    }
-                    catch (Throwable var14)
-                    {
-                        var15.addSuppressed(var14);
-                    }
-
-                    throw var15;
-                }
-
-                input.close();
-            }
-            finally
-            {
-                conn.disconnect();
-            }
-
-            Log.getLogger().log(Level.INFO, "Patreon Whitelist validating, request content: " + content);
-            return statusCode == 200 && Boolean.parseBoolean(content);
-        }
-        catch (IOException var17)
-        {
-            var17.printStackTrace();
-            return false;
-        }
+        return PatreonWhitelist.config.checkAuth(profile);
     }
 }
